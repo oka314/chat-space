f8388d3805a713048c9d4248a51a208db04774e6
oka314 <61148080+oka314@users.noreply.github.com>
2020-02-27 14:42:05 +0900

    岡本

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+       var html =
+          `<div class= "message-date__log">
+            <div class= "user">
+              ${message.user_name}
+            </div>
+            <div class= "date">
+              ${message.created_at}
+            </div>
+          </div>  
+          <div class="message-date__comment">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >`
+      return html;
+    } else {
+      var html =
+      ` <div class="message-date__log">
+          <div class="user">
+            ${message.user_name}
+          </div>
+          <div class="date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-date__comment">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>`
+     return html;
+    };
+   }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax ({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message-date').append(html);
+      $('.message-date').animate({ scrollTop: $('.message-date')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+  })
+ });
